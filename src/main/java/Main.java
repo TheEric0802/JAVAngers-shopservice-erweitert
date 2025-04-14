ddcd292aec3cc63706d48c9981b750ec0d14e0f3
Eric Schmidt <eric0802@web.de>
2025-04-14 17:22:58 +0200

    add feature "Transaction File"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -17,5 +23,43 @@ public class Main {
         shopService.addOrder(List.of("1", "2", "3", "4"));
 
         System.out.println(shopService.getOrdersByStatus(OrderStatus.PROCESSING));
+
+        System.out.println("------------------------");
+
+        ProductRepo productRepo2 = new ProductRepo();
+        productRepo2.addProduct(new Product("1", "Apfel"));
+        productRepo2.addProduct(new Product("2", "Birne"));
+        productRepo2.addProduct(new Product("3", "Erdbeere"));
+        productRepo2.addProduct(new Product("4", "Kirsche"));
+        OrderRepo orderRepo2 = new OrderMapRepo();
+        IdService idService2 = new IdService();
+        ShopService shopService2 = new ShopService(productRepo2, orderRepo2, idService2);
+        Map<String, String> aliasMap = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("src/transactions.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                switch (parts[0]) {
+                    case "addOrder":
+                        String alias = parts[1];
+                        List<String> productIds = Arrays.stream(parts).skip(2).toList();
+                        aliasMap.put(alias, shopService2.addOrder(productIds).id());
+                        break;
+                    case "setStatus":
+                        shopService2.updateOrder(aliasMap.get(parts[1]), OrderStatus.valueOf(parts[2]));
+                        break;
+                    case "printOrders":
+                        for (Map.Entry<String, String> entry : aliasMap.entrySet()) {
+                            System.out.println("Order: " + entry.getKey());
+                            System.out.println(orderRepo2.getOrderById(entry.getValue()));
+                        }
+                        break;
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
